0ac50975a9a578ad0addd4adc293724ed976777c
Alex <alexandr.dubiaga@gmail.com>
2020-03-31 11:56:57 +0300

    first coms

---
+++ Assets/MouseHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseHandler : MonoBehaviour
+{
+    [Header("Main conf")]
+    public int[] Array;
+    public float Speed;
+    [Header("Second conf")]
+    public GameObject Object;
+    public TestData Data;
+    [Space(100)]
+    [SerializeField]
+    private List<TestData> Datas;
+    [HideInInspector]
+    public string Text;
+
+}
+[System.Serializable]
+public class TestData
+{
+    public float Heals;
+    public float Money;
+
+}
--- Assets/main.cs
+++ Assets/main.cs
@@ -2,16 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class main : MonoBehaviour
+public class Main : MonoBehaviour
 {
+    private void Awake()
+    {
+        Debug.Log("Awake " + gameObject.name);
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Start " + gameObject.name);
     }
 
     // Update is called once per frame
     void Update()
+    {
+        Debug.Log("Update " + gameObject.name);
+    }
+    private void FixedUpdate()
+    {
+        Debug.Log("FixedUpdate " + gameObject.name);
+    }
+    private void LateUpdate()
     {
         
     }
